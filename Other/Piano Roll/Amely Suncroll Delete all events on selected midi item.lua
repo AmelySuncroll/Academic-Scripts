71100db4c7b7e833f374a45dadd8879d0d85e890
Amely Suncroll <164076705+AmelySuncroll@users.noreply.github.com>
2024-05-12 13:07:48 +0400

    Update Amely Suncroll Delete all events on selected midi item.lua

--- Other/Piano Roll/Amely Suncroll Delete all events on selected midi item.lua
+++ Other/Piano Roll/Amely Suncroll Delete all events on selected midi item.lua
@@ -1,27 +1,22 @@
---[[
+-- @description Delete all events on selected midi item
+-- @author Amely Suncroll
+-- @version 1.0
+-- @website https://forum.cockos.com/showthread.php?t=291012
+-- @changelog
+--    + init @
+-- @about Get piano roll and call the script to delete all events such as velocity (will be 100), pitch etc
 
-  ReaScript Name: Delete all events on selected midi item
-  Instructions: Just open it with Actions -  New action - Load ReaScript
-  Author: Amely Suncroll
-  REAPER: 6+ (maybe less)
-  Extensions: none
-  Version: 1.0
+-- @donation https://www.paypal.com/paypalme/suncroll
 
-  About: get piano roll and call the script to delete all events such as velocity (will be 100), pitch etc
+-- @website: https://t.me/reaper_ua
 
+-- Support:
+-- https://t.me/yxo_composer_support
+-- amelysuncroll@gmail.com
 
-  Donations: 
-  https://www.paypal.com/paypalme/suncroll
-
-  Support:
-  https://t.me/yxo_composer_support
-  amelysuncroll@gmail.com
-
-  Other links:
-  https://github.com/AmelySuncroll
-  https://www.youtube.com/@yxo_composer
-
-]]--
+-- Other links:
+-- https://github.com/AmelySuncroll
+-- https://www.youtube.com/@yxo_composer
 
 
 
