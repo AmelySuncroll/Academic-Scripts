996bdb12e531747de1da01abd172325fca6da8ca
Amely Suncroll <164076705+AmelySuncroll@users.noreply.github.com>
2024-05-12 13:03:46 +0400

    Update Amely Suncroll Select all items after edit cursor.lua

--- Other/Arrange View/Amely Suncroll Select all items after edit cursor.lua
+++ Other/Arrange View/Amely Suncroll Select all items after edit cursor.lua
@@ -1,27 +1,24 @@
---[[
+-- @description Select all items after edit cursor
+-- @author Amely Suncroll
+-- @version 1.0
+-- @website https://forum.cockos.com/showthread.php?t=291012
+-- @changelog
+--    + init @
+-- @about Select all items after edit cursor
+-- inspired by DaVinci Resolve
 
-  ReaScript Name: Select all items after edit cursor
-  Instructions: Just open it with Actions -  New action - Load ReaScript
-  Author: Amely Suncroll
-  REAPER: 6+ (maybe less)
-  Extensions: none
-  Version: 1.0
+-- @donation https://www.paypal.com/paypalme/suncroll
 
-  About: Select all items after edit cursor.
+-- @website: https://t.me/reaper_ua
 
+-- Support:
+-- https://t.me/yxo_composer_support
+-- amelysuncroll@gmail.com
 
-  Donations: 
-  https://www.paypal.com/paypalme/suncroll
+-- Other links:
+-- https://github.com/AmelySuncroll
+-- https://www.youtube.com/@yxo_composer
 
-  Support:
-  https://t.me/yxo_composer_support
-  amelysuncroll@gmail.com
-
-  Other links:
-  https://github.com/AmelySuncroll
-  https://www.youtube.com/@yxo_composer
-
-]]--
 
 local track_count = reaper.CountTracks(0)
 
