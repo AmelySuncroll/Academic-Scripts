d079fd70040aeb9a2176d74de6036a3b8d08a465
Amely Suncroll <164076705+AmelySuncroll@users.noreply.github.com>
2024-05-12 09:44:31 +0400

    Add files via upload

---
+++ Academic Scripts/Arrange View/Amely Suncroll Academic Piano.lua
@@ -0,0 +1,88 @@
+--[[
+
+  ReaScript Name: Academic Piano
+  Instructions: Just open it with Actions -  New action - Load ReaScript
+  Author: Amely Suncroll
+  REAPER: 6+ (maybe less)
+  Extensions: none
+  Version: 1.0
+
+  About: Play on your pc keyboard anywhere (you need to add this script to Arrange View (Main) and Piano Roll Actions separately)
+
+  IF YOU HAVE NO SOUND:
+  1. Click right mouse on Virtual MIDI Keyboard (VKB) and check 
+  "Send all keyboard input to VKB (even when other windows active)";
+
+  2. Be sure your MIDI-channel on VKB is set to 01 (check right virtual keyboard corner);
+
+  3 Be sure your MIDI-channel in your VST is set to 01 or "omni".
+
+  4.1 Check your preferences - Project - Track/Send Defaults - Record config: "MIDI: Virtual MIDI keyboard: All channels"
+  or
+  4.2 Click on input part of track - Input: MIDI - Virtual MIDI keyboard - All channels (this option is local to selected track)
+
+
+
+  Donations: 
+  https://www.paypal.com/paypalme/suncroll
+
+  Support:
+  https://t.me/yxo_composer_support
+  amelysuncroll@gmail.com
+
+  Other links:
+  https://github.com/AmelySuncroll
+  https://www.youtube.com/@yxo_composer
+
+]]--
+
+local extStateSection = "MyCustomScript"
+local extStateKey = "isCommandsActive"
+local cmdSNMInputAllCh = reaper.NamedCommandLookup("_S&M_MIDI_INPUT_ALL_CH")
+local cmdSNMMapInputCh1 = reaper.NamedCommandLookup("_S&M_MAP_MIDI_INPUT_CH1")
+
+function RunCommands()
+  local state
+  
+  state = reaper.GetToggleCommandState(40740)
+  if state ~= 1 then reaper.Main_OnCommand(40740, 0) end
+  
+  state = reaper.GetToggleCommandState(40637)
+  if state ~= 1 then reaper.Main_OnCommand(40637, 0) end
+  
+  state = reaper.GetToggleCommandState(40377)
+  if state ~= 1 then reaper.Main_OnCommand(40377, 0) end
+  
+  reaper.Main_OnCommand(cmdSNMInputAllCh, 0)
+  reaper.Main_OnCommand(cmdSNMMapInputCh1, 0)
+end
+
+function ToggleCommands()
+  reaper.Main_OnCommand(40740, 0)
+  reaper.Main_OnCommand(40637, 0)
+  reaper.Main_OnCommand(40377, 0)
+  reaper.Main_OnCommand(cmdSNMInputAllCh, 0)
+  reaper.Main_OnCommand(cmdSNMMapInputCh1, 0)
+end
+
+
+function SetScriptState(state)
+  reaper.SetExtState(extStateSection, extStateKey, tostring(state), false)
+end
+
+function GetScriptState()
+  return reaper.GetExtState(extStateSection, extStateKey) == "true"
+end
+
+if not GetScriptState() then
+  RunCommands()
+  SetScriptState(true)
+  reaper.MB("Piano mode is on", "Academic Piano", 0)
+else
+  RunCommands()
+  ToggleCommands()
+  SetScriptState(false)
+  reaper.MB("Piano mode is off", "Academic Piano", 0)
+end
+
+reaper.UpdateArrange()
---
+++ Academic Scripts/Piano roll/Amely Suncroll Academic Pitch 1.11.lua
@@ -0,0 +1,218 @@
+--[[
+
+  ReaScript Name: Academic Pitch
+  Instructions: Just open it with Actions -  New action - Load ReaScript
+  Author: Amely Suncroll
+  REAPER: 6+ (maybe less)
+  Extensions: none
+  Version: 1.11
+
+  About: 
+
+  b = move pitch bend down by one semitone
+  # = move pitch bend up by one semitone
+
+  /b = the half of b 
+  /# = the half of #
+  
+  you can type #/# and b/b, bb and ## also
+  b (or #) nat = natural b or # (before Bach)
+
+  v1.1: add pitch points to each note before input
+  v1.11: keep midi editor in focus
+
+  Запрошую долучитися: https://t.me/reaper_ua
+
+  Donations: 
+  https://www.paypal.com/paypalme/suncroll
+
+  Support:
+  https://t.me/yxo_composer_support
+  amelysuncroll@gmail.com
+
+  Other links:
+  https://github.com/AmelySuncroll
+  https://www.youtube.com/@yxo_composer
+
+]]--
+
+local reaper = reaper
+
+local function switchToPitch()
+    reaper.MIDIEditor_LastFocused_OnCommand(40366, false)
+end
+
+function saveNoteSelection(take)
+    local _, noteCount = reaper.MIDI_CountEvts(take)
+    local selection = {}
+    for i = 0, noteCount - 1 do
+        local _, selected, _, _, _, _, _, _ = reaper.MIDI_GetNote(take, i)
+        table.insert(selection, selected)
+    end
+    return selection
+end
+
+function restoreNoteSelection(take, selection)
+    local _, noteCount = reaper.MIDI_CountEvts(take)
+    for i = 0, noteCount - 1 do
+        reaper.MIDI_SetNote(take, i, selection[i + 1], nil, nil, nil, nil, nil, nil, true)
+    end
+end
+
+function insertPitchBendOnSelectedNotesIfNotExist(take)
+    local initialSelection = saveNoteSelection(take) 
+
+    local _, noteCount = reaper.MIDI_CountEvts(take)
+    for i = 0, noteCount - 1 do
+        local _, selected, _, startppqpos, endppqpos, _, _, _ = reaper.MIDI_GetNote(take, i)
+        if selected and not pitchBendExists(take, startppqpos, endppqpos) then
+            reaper.MIDI_InsertCC(take, false, false, startppqpos, 0xE0, 0, 64, 64) 
+        end
+    end
+
+    restoreNoteSelection(take, initialSelection) 
+end
+
+function pitchBendExists(take, startppqpos, endppqpos)
+    local _, ccCount = reaper.MIDI_CountEvts(take)
+    for i = 0, ccCount - 1 do
+        local _, _, _, ppqpos, chanmsg, _, _, _ = reaper.MIDI_GetCC(take, i)
+        if chanmsg == 224 and ppqpos >= startppqpos and ppqpos <= endppqpos then
+            return true 
+        end
+    end
+    return false 
+end
+
+function selectAllNotes(take)
+    local _, noteCount = reaper.MIDI_CountEvts(take)
+    for i = 0, noteCount - 1 do
+        reaper.MIDI_SetNote(take, i, true, nil, nil, nil, nil, nil, nil, true) 
+    end
+end
+
+function main()
+    local editor = reaper.MIDIEditor_GetActive() 
+    if editor == nil then return end
+    local take = reaper.MIDIEditor_GetTake(editor) 
+    if take == nil then return end
+
+    reaper.Undo_BeginBlock() 
+    reaper.MIDI_DisableSort(take) 
+
+    local initialSelection = saveNoteSelection(take) 
+    selectAllNotes(take)
+    insertPitchBendOnSelectedNotesIfNotExist(take)
+    restoreNoteSelection(take, initialSelection)
+
+    reaper.MIDI_Sort(take) 
+    reaper.Undo_EndBlock("Insert Pitch Bend with Inverted Selection", -1) 
+end
+
+main()
+
+
+
+local pitchBendValues = {
+    ["bb"] = 0,
+    ["b/b"] = 2048,
+    ["b"] = 4096,
+    ["b nat"] = 5460,
+    ["/b"] = 6144,
+    ["0"] = 8192,
+    ["/#"] = 10240,
+    ["# nat"] = 10922,
+    ["#"] = 12288,
+    ["#/#"] = 14336,
+    ["##"] = 16383
+}
+
+local function deletePreviousPitchBend(take, startppqpos, endppqpos)
+    local _, countCC, _, _ = reaper.MIDI_CountEvts(take)
+    for i = countCC - 1, 0, -1 do
+        local _, _, _, ppqpos, _, _, _, _ = reaper.MIDI_GetCC(take, i)
+        if ppqpos >= startppqpos and ppqpos <= endppqpos then
+            reaper.MIDI_DeleteCC(take, i)
+        end
+    end
+end
+
+local command_id = 40153
+
+local function shortenSelectedNotes()
+    reaper.MIDIEditor_LastFocused_OnCommand(40445, false)
+end
+
+local function lengthenSelectedNotes()
+    reaper.MIDIEditor_LastFocused_OnCommand(40444, false)
+end
+
+local function applyPitchBend(bendValue)
+    local itemCount = reaper.CountSelectedMediaItems(0)
+    if itemCount == 0 then
+        reaper.ShowMessageBox("No notes are selected. Please select some notes and try again.", ":(", 0)
+        return
+    end
+    
+    local lastNoteEndPPQPos = nil
+
+    for i = 0, itemCount - 1 do
+        local item = reaper.GetSelectedMediaItem(0, i)
+        local take = reaper.GetActiveTake(item)
+        if take and reaper.TakeIsMIDI(take) then
+            local _, noteCount, _, _ = reaper.MIDI_CountEvts(take)
+            for j = 0, noteCount - 1 do
+                local _, selected, _, startppqpos, endppqpos, _, _, _ = reaper.MIDI_GetNote(take, j)
+                if selected then
+                    deletePreviousPitchBend(take, startppqpos, endppqpos)
+                    reaper.MIDI_InsertCC(take, false, false, startppqpos, 0xE0, 0, bendValue & 0x7F, (bendValue >> 7) & 0x7F)
+                    lastNoteEndPPQPos = math.max(lastNoteEndPPQPos or 0, endppqpos)
+                end
+            end
+        end
+    end
+    
+    reaper.UpdateArrange()
+end
+
+local function getUserInput()
+    local retval, userInput = reaper.GetUserInputs("Academic Pitch 1.11", 1, "Enter pitch value or accidentals:", "")
+    if not retval then
+        return nil 
+    end
+
+    local bendValue = pitchBendValues[userInput] or tonumber(userInput)
+
+    if bendValue and bendValue >= 0 and bendValue <= 16383 then
+        return bendValue 
+    else
+        reaper.ShowMessageBox("Invalid input! \n \nPlease enter a valid number between 0 and 16383 or next accidentals: bb, b/b, b, /b, 0, /#, #, #/#, ##;  also b or # nat.", ":(", 0)
+        return false
+    end
+end
+
+function focusMidiEditor()
+  local focus_midi_editor = reaper.NamedCommandLookup("_SN_FOCUS_MIDI_EDITOR")
+  reaper.Main_OnCommand(focus_midi_editor, 0)
+end
+
+local function main()    
+    switchToPitch()
+    shortenSelectedNotes()
+
+    while true do
+        local bendValue = getUserInput()
+        if bendValue == nil then 
+            lengthenSelectedNotes()
+            return
+        elseif bendValue ~= false then 
+            applyPitchBend(bendValue)
+            break
+        end
+    end
+
+    lengthenSelectedNotes()
+end
+
+main()
+focusMidiEditor()
