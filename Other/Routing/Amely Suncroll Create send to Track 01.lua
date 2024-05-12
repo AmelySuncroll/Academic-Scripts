0ad8af887096a5c78fd27f69e185a4398b3bf100
Amely Suncroll <164076705+AmelySuncroll@users.noreply.github.com>
2024-05-12 13:22:29 +0400

    Update Amely Suncroll Create send to Track 01.lua

--- Other/Routing/Amely Suncroll Create send to Track 01.lua
+++ Other/Routing/Amely Suncroll Create send to Track 01.lua
@@ -1,27 +1,22 @@
---[[
+-- @description Create send to first project track
+-- @author Amely Suncroll
+-- @version 1.0
+-- @website https://forum.cockos.com/showthread.php?t=291012
+-- @changelog
+--    + init @
+-- @about Create send to the FIRST track (include folders)
 
-  ReaScript Name: Create send to ... 
-  Instructions: Just open it with Actions -  New action - Load ReaScript
-  Author: Amely Suncroll
-  REAPER: 6+ (maybe less)
-  Extensions: none
-  Version: 1.0
+-- @donation https://www.paypal.com/paypalme/suncroll
 
-  About: create send to one of your track with one button.
+-- @website: https://t.me/reaper_ua
 
+-- Support:
+-- https://t.me/yxo_composer_support
+-- amelysuncroll@gmail.com
 
-  Donations: 
-  https://www.paypal.com/paypalme/suncroll
-
-  Support:
-  https://t.me/yxo_composer_support
-  amelysuncroll@gmail.com
-
-  Other links:
-  https://github.com/AmelySuncroll
-  https://www.youtube.com/@yxo_composer
-
-]]--
+-- Other links:
+-- https://github.com/AmelySuncroll
+-- https://www.youtube.com/@yxo_composer
 
 function dBToSlider(dB)
     return 10 ^ (dB / 20)
