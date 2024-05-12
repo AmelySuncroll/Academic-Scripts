9b652232ef1523b8b31413e1cd6bcda2847827ad
Amely Suncroll <164076705+AmelySuncroll@users.noreply.github.com>
2024-05-12 13:04:05 +0400

    Update Amely Suncroll Select all items before edit cursor.lua

--- Other/Arrange View/Amely Suncroll Select all items before edit cursor.lua
+++ Other/Arrange View/Amely Suncroll Select all items before edit cursor.lua
@@ -1,27 +1,23 @@
---[[
+-- @description Select all items before edit cursor
+-- @author Amely Suncroll
+-- @version 1.0
+-- @website https://forum.cockos.com/showthread.php?t=291012
+-- @changelog
+--    + init @
+-- @about Select all items before edit cursor
+-- inspired by DaVinci Resolve
 
-  ReaScript Name: Select all items before edit cursor
-  Instructions: Just open it with Actions -  New action - Load ReaScript
-  Author: Amely Suncroll
-  REAPER: 6+ (maybe less)
-  Extensions: none
-  Version: 1.0
+-- @donation https://www.paypal.com/paypalme/suncroll
 
-  About: select all items before edit cursor.
+-- @website: https://t.me/reaper_ua
 
+-- Support:
+-- https://t.me/yxo_composer_support
+-- amelysuncroll@gmail.com
 
-  Donations: 
-  https://www.paypal.com/paypalme/suncroll
-
-  Support:
-  https://t.me/yxo_composer_support
-  amelysuncroll@gmail.com
-
-  Other links:
-  https://github.com/AmelySuncroll
-  https://www.youtube.com/@yxo_composer
-
-]]--
+-- Other links:
+-- https://github.com/AmelySuncroll
+-- https://www.youtube.com/@yxo_composer
 
 local track_count = reaper.CountTracks(0)
 
