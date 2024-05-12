0cceb1decf451c30135b438810ec9d8a09238bc4
Amely Suncroll <164076705+AmelySuncroll@users.noreply.github.com>
2024-05-12 13:17:11 +0400

    Update Amely Suncroll Toggle metronome in piano roll.lua

--- Other/Piano Roll/Amely Suncroll Toggle metronome in piano roll.lua
+++ Other/Piano Roll/Amely Suncroll Toggle metronome in piano roll.lua
@@ -1,27 +1,22 @@
---[[
+-- @description Toggle metronome in piano roll
+-- @author Amely Suncroll
+-- @version 1.0
+-- @website https://forum.cockos.com/showthread.php?t=291012
+-- @changelog
+--    + init @
+-- @about Turn on/off metronome inside piano roll (toggle)
 
-  ReaScript Name: Toggle metronome in piano roll
-  Instructions: Just open it with Actions -  New action - Load ReaScript
-  Author: Amely Suncroll
-  REAPER: 6+ (maybe less)
-  Extensions: none
-  Version: 1.0
+-- @donation https://www.paypal.com/paypalme/suncroll
 
-  About: just metronome.
+-- @website: https://t.me/reaper_ua
 
+-- Support:
+-- https://t.me/yxo_composer_support
+-- amelysuncroll@gmail.com
 
-  Donations: 
-  https://www.paypal.com/paypalme/suncroll
-
-  Support:
-  https://t.me/yxo_composer_support
-  amelysuncroll@gmail.com
-
-  Other links:
-  https://github.com/AmelySuncroll
-  https://www.youtube.com/@yxo_composer
-
-]]--
+-- Other links:
+-- https://github.com/AmelySuncroll
+-- https://www.youtube.com/@yxo_composer
 
 local midiEditor = reaper.MIDIEditor_GetActive()
 if midiEditor ~= nil then
