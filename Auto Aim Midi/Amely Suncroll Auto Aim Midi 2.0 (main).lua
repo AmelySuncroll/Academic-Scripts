8e351fa45e85e52078855967648e202909cc32c3
Amely Suncroll <164076705+AmelySuncroll@users.noreply.github.com>
2024-06-08 22:06:41 +0400

    Update Amely Suncroll Auto Aim Midi 2.0 (main).lua

--- Auto Aim Midi/Amely Suncroll Auto Aim Midi 2.0 (main).lua
+++ Auto Aim Midi/Amely Suncroll Auto Aim Midi 2.0 (main).lua
@@ -1,35 +1,19 @@
---[[
+-- @description Auto Aim Midi (main)
+-- @author Amely Suncroll
+-- @version 2.0
+-- @website https://forum.cockos.com/showthread.php?t=291012
+-- @changelog
+--    + 2.0 add toggle script
 
-  ReaScript Name: Auto Aim Midi (main)
-  Instructions: Just open it with Actions -  New action - Load ReaScript
-  Author: Amely Suncroll
-  REAPER: 6+ (maybe less)
-  Extensions: none
-  Version: 2.0
+-- @about Glue edit cursor to start or end of selected note or notes. It has the main script and the switch scriipt. Best solution for working with foleys.
 
-  About: An exact copy of the Auto Aim Item 2.0 script, but applied to items. 
-  The kit consists of two components - the "main" and the so-called "switch", or "toggle". 
-  When you run the "main", it starts running in the background, while the "toggle" at this time 
-  changes the edit cursor binding of the selected note to its beginning or end. 
-  Once you're done with your work, simply turn off the main part of the script from the background. 
-         
-  I recommend use "toggle" script as a mouse modifier to easy to use:
-  Options - Preferences - Mouse Modifiers - MIDI Note - Double Click - Default Action - Action list
-    
+-- @donation https://www.paypal.com/paypalme/suncroll
 
-  Donations: 
-  https://www.paypal.com/paypalme/suncroll
-  if you are from russia - text me by links below
+-- @website https://t.me/reaper_ua
 
-  Support:
-  https://t.me/yxo_composer_support
-  amelysuncroll@gmail.com
-
-  Other links:
-  https://github.com/AmelySuncroll
-  https://www.youtube.com/@yxo_composer
-
-]]
+-- Support:
+-- https://t.me/yxo_composer_support
+-- amelysuncroll@gmail.com
 
 local useEndOfNote = false 
 
