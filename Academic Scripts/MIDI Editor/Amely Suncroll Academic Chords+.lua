016ce3d7a8b49d0177f4a53c6e87c82bd30c1e87
Amely Suncroll <164076705+AmelySuncroll@users.noreply.github.com>
2024-06-08 23:39:11 +0400

    Update Amely Suncroll Academic Chords+.lua

--- Academic Scripts/MIDI Editor/Amely Suncroll Academic Chords+.lua
+++ Academic Scripts/MIDI Editor/Amely Suncroll Academic Chords+.lua
@@ -4,7 +4,7 @@
 -- @website https://forum.cockos.com/showthread.php?t=291012
 -- @changelog
 --    + init @
--- @about test test test
+-- @about test test test 
 
 local function calculateTotalDuration(note_lengths)
   local total = 0
