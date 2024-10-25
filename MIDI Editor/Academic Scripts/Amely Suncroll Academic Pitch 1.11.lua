391a30564817166e5dcb018889fb17b121b739c8
Amely Suncroll <164076705+AmelySuncroll@users.noreply.github.com>
2024-10-25 15:20:33 +0400

    Update Amely Suncroll Academic Pitch 1.11.lua

--- MIDI Editor/Academic Scripts/Amely Suncroll Academic Pitch 1.11.lua
+++ MIDI Editor/Academic Scripts/Amely Suncroll Academic Pitch 1.11.lua
@@ -1,11 +1,12 @@
 -- @description Academic Pitch
 -- @author Amely Suncroll
--- @version 1.11
+-- @version 2.0
 -- @website https://forum.cockos.com/showthread.php?t=291012
 -- @changelog
 --    + init @
 --    + v1.1 add pitch points to all notes before using script
 --    + v.11 keep midi editor focused
+--    + 2.0 add random function and four commands (r; r X; r X Y; r b #), add slide function (X to Y), change 'b nat' and '# nat' to 'bn' and '#n'
 -- @about Use academic symbols such as "b" or "#" to change a pitch. You can get more with combinate them - bb, ##, /b, /#, b/b or #/#. Use b nat or # nat. Use numeral values from 1 to 16383.
     
 -- @donation https://www.paypal.com/paypalme/suncroll
@@ -18,9 +19,24 @@
 
 -- Other links:
 -- https://github.com/AmelySuncroll
--- https://www.youtube.com/@yxo_composer
 
 local reaper = reaper
+
+math.randomseed(os.time())
+
+local pitchBendValues = {
+    ["bb"] = 0,
+    ["b/b"] = 2048,
+    ["b"] = 4096,
+    ["bn"] = 5460,
+    ["/b"] = 6144,
+    ["0"] = 8192,
+    ["/#"] = 10240,
+    ["#n"] = 10922,
+    ["#"] = 12288,
+    ["#/#"] = 14336,
+    ["##"] = 16383
+}
 
 local function switchToPitch()
     reaper.MIDIEditor_LastFocused_OnCommand(40366, false)
@@ -30,7 +46,7 @@ function saveNoteSelection(take)
     local _, noteCount = reaper.MIDI_CountEvts(take)
     local selection = {}
     for i = 0, noteCount - 1 do
-        local _, selected, _, _, _, _, _, _ = reaper.MIDI_GetNote(take, i)
+        local _, selected = reaper.MIDI_GetNote(take, i)
         table.insert(selection, selected)
     end
     return selection
@@ -48,8 +64,8 @@ function insertPitchBendOnSelectedNotesIfNotExist(take)
 
     local _, noteCount = reaper.MIDI_CountEvts(take)
     for i = 0, noteCount - 1 do
-        local _, selected, _, startppqpos, endppqpos, _, _, _ = reaper.MIDI_GetNote(take, i)
-        if selected and not pitchBendExists(take, startppqpos, endppqpos) then
+        local _, selected, _, startppqpos = reaper.MIDI_GetNote(take, i)
+        if selected and not pitchBendExists(take, startppqpos) then
             reaper.MIDI_InsertCC(take, false, false, startppqpos, 0xE0, 0, 64, 64) 
         end
     end
@@ -57,71 +73,38 @@ function insertPitchBendOnSelectedNotesIfNotExist(take)
     restoreNoteSelection(take, initialSelection) 
 end
 
-function pitchBendExists(take, startppqpos, endppqpos)
+function pitchBendExists(take, ppqpos)
     local _, ccCount = reaper.MIDI_CountEvts(take)
     for i = 0, ccCount - 1 do
-        local _, _, _, ppqpos, chanmsg, _, _, _ = reaper.MIDI_GetCC(take, i)
-        if chanmsg == 224 and ppqpos >= startppqpos and ppqpos <= endppqpos then
+        local _, _, _, cc_ppqpos, chanmsg = reaper.MIDI_GetCC(take, i)
+        if chanmsg == 224 and cc_ppqpos == ppqpos then
             return true 
         end
     end
     return false 
 end
 
-function selectAllNotes(take)
-    local _, noteCount = reaper.MIDI_CountEvts(take)
-    for i = 0, noteCount - 1 do
-        reaper.MIDI_SetNote(take, i, true, nil, nil, nil, nil, nil, nil, true) 
-    end
-end
-
-function main()
-    local editor = reaper.MIDIEditor_GetActive() 
-    if editor == nil then return end
-    local take = reaper.MIDIEditor_GetTake(editor) 
-    if take == nil then return end
-
-    reaper.Undo_BeginBlock() 
-    reaper.MIDI_DisableSort(take) 
-
-    local initialSelection = saveNoteSelection(take) 
-    selectAllNotes(take)
-    insertPitchBendOnSelectedNotesIfNotExist(take)
-    restoreNoteSelection(take, initialSelection)
-
-    reaper.MIDI_Sort(take) 
-    reaper.Undo_EndBlock("Insert Pitch Bend with Inverted Selection", -1) 
-end
-
-main()
-
-
-
-local pitchBendValues = {
-    ["bb"] = 0,
-    ["b/b"] = 2048,
-    ["b"] = 4096,
-    ["b nat"] = 5460,
-    ["/b"] = 6144,
-    ["0"] = 8192,
-    ["/#"] = 10240,
-    ["# nat"] = 10922,
-    ["#"] = 12288,
-    ["#/#"] = 14336,
-    ["##"] = 16383
-}
-
-local function deletePreviousPitchBend(take, startppqpos, endppqpos)
-    local _, countCC, _, _ = reaper.MIDI_CountEvts(take)
+local function deletePreviousPitchBend(take, ppqpos)
+    local _, countCC = reaper.MIDI_CountEvts(take)
     for i = countCC - 1, 0, -1 do
-        local _, _, _, ppqpos, _, _, _, _ = reaper.MIDI_GetCC(take, i)
-        if ppqpos >= startppqpos and ppqpos <= endppqpos then
+        local _, _, _, cc_ppqpos, chanmsg = reaper.MIDI_GetCC(take, i)
+        if chanmsg == 224 and cc_ppqpos == ppqpos then
             reaper.MIDI_DeleteCC(take, i)
         end
     end
 end
 
-local command_id = 40153
+local function getExistingPitchBendValue(take, ppqpos)
+    local _, ccCount = reaper.MIDI_CountEvts(take)
+    for i = ccCount - 1, 0, -1 do
+        local _, _, _, cc_ppqpos, chanmsg, _, msg2, msg3 = reaper.MIDI_GetCC(take, i)
+        if chanmsg == 224 and cc_ppqpos <= ppqpos then
+            local bendValue = msg2 + (msg3 << 7)
+            return bendValue
+        end
+    end
+    return 8192 -- If there is no existing pitch bend, return the default value
+end
 
 local function shortenSelectedNotes()
     reaper.MIDIEditor_LastFocused_OnCommand(40445, false)
@@ -131,66 +114,196 @@ local function lengthenSelectedNotes()
     reaper.MIDIEditor_LastFocused_OnCommand(40444, false)
 end
 
-local function applyPitchBend(bendValue)
-    local itemCount = reaper.CountSelectedMediaItems(0)
-    if itemCount == 0 then
-        reaper.ShowMessageBox("No notes are selected. Please select some notes and try again.", ":(", 0)
+local function parsePitchValue(input)
+    input = input:lower()
+    input = input:gsub("%s+", "")
+    local value = pitchBendValues[input]
+    if value then
+        return value
+    else
+        value = tonumber(input)
+        if value and value >= 0 and value <= 16383 then
+            return value
+        else
+            return nil
+        end
+    end
+end
+
+local function applyPitchBend(bendValueData)
+    local editor = reaper.MIDIEditor_GetActive()
+    if not editor then
+        reaper.ShowMessageBox("Failed to get active MIDI editor.", ":(", 0)
         return
     end
-    
-    local lastNoteEndPPQPos = nil
 
-    for i = 0, itemCount - 1 do
-        local item = reaper.GetSelectedMediaItem(0, i)
-        local take = reaper.GetActiveTake(item)
-        if take and reaper.TakeIsMIDI(take) then
-            local _, noteCount, _, _ = reaper.MIDI_CountEvts(take)
-            for j = 0, noteCount - 1 do
-                local _, selected, _, startppqpos, endppqpos, _, _, _ = reaper.MIDI_GetNote(take, j)
-                if selected then
-                    deletePreviousPitchBend(take, startppqpos, endppqpos)
-                    reaper.MIDI_InsertCC(take, false, false, startppqpos, 0xE0, 0, bendValue & 0x7F, (bendValue >> 7) & 0x7F)
-                    lastNoteEndPPQPos = math.max(lastNoteEndPPQPos or 0, endppqpos)
+    local take = reaper.MIDIEditor_GetTake(editor)
+    if not take then
+        reaper.ShowMessageBox("Failed to get active take.", ":(", 0)
+        return
+    end
+
+    local _, noteCount = reaper.MIDI_CountEvts(take)
+    local selectedNoteExists = false
+    for i = 0, noteCount - 1 do
+        local _, selected = reaper.MIDI_GetNote(take, i)
+        if selected then
+            selectedNoteExists = true
+            break
+        end
+    end
+
+    if not selectedNoteExists then
+        reaper.ShowMessageBox("No selected notes. Please select notes and try again.", ":(", 0)
+        return
+    end
+
+    local selectedNoteIndex = 0
+    local totalSelectedNotes = 0
+    for i = 0, noteCount - 1 do
+        local _, selected = reaper.MIDI_GetNote(take, i)
+        if selected then
+            totalSelectedNotes = totalSelectedNotes + 1
+        end
+    end
+
+    for i = 0, noteCount - 1 do
+        local _, selected, _, startppqpos = reaper.MIDI_GetNote(take, i)
+        if selected then
+            deletePreviousPitchBend(take, startppqpos)
+
+            local newBendValue
+
+            if bendValueData.type == 'fixed' then
+                newBendValue = bendValueData.value
+            elseif bendValueData.type == 'random' then
+                local minValue, maxValue
+
+                if bendValueData.stepsDownMax and bendValueData.stepsUpMax then
+                    local stepsDownMax = bendValueData.stepsDownMax
+                    local stepsUpMax = bendValueData.stepsUpMax
+
+                    minValue = stepsDownMax
+                    maxValue = stepsUpMax
+                elseif bendValueData.minValue and bendValueData.maxValue then
+                    minValue = bendValueData.minValue
+                    maxValue = bendValueData.maxValue
+                else
+                    minValue = 0
+                    maxValue = 16383
                 end
+
+                -- Swap values if necessary
+                if minValue > maxValue then
+                    minValue, maxValue = maxValue, minValue
+                end
+
+                newBendValue = math.random(math.floor(minValue), math.floor(maxValue))
+            elseif bendValueData.type == 'slide' then
+                local startValue = bendValueData.startValue
+                local endValue = bendValueData.endValue
+                newBendValue = math.floor(startValue + ((endValue - startValue) * (selectedNoteIndex / (totalSelectedNotes - 1))))
+                selectedNoteIndex = selectedNoteIndex + 1
+            end
+
+            if selected then
+                reaper.MIDI_InsertCC(take, false, false, startppqpos, 0xE0, 0, newBendValue & 0x7F, (newBendValue >> 7) & 0x7F)
             end
         end
     end
-    
+
     reaper.UpdateArrange()
 end
 
 local function getUserInput()
-    local retval, userInput = reaper.GetUserInputs("Academic Pitch 1.11", 1, "Enter pitch value or accidentals:", "")
+    local retval, userInput = reaper.GetUserInputs("Academic Pitch 2.0", 1, "Enter pitch value or accidentals:", "")
     if not retval then
         return nil 
     end
 
-    local bendValue = pitchBendValues[userInput] or tonumber(userInput)
+    userInput = userInput:lower()
 
-    if bendValue and bendValue >= 0 and bendValue <= 16383 then
-        return bendValue 
+    local params = {}
+    for word in userInput:gmatch("%S+") do
+        table.insert(params, word)
+    end
+
+    if params[1] == 'r' then
+        if #params == 3 then
+            -- Handling 'r X Y'
+            local xInput = params[2]
+            local yInput = params[3]
+            local stepsDownMax = parsePitchValue(xInput)
+            local stepsUpMax = parsePitchValue(yInput)
+
+            if stepsDownMax and stepsUpMax then
+                return {type = 'random', stepsDownMax = stepsDownMax, stepsUpMax = stepsUpMax}
+            else
+                reaper.ShowMessageBox("Invalid input! \n\nPlease enter the command in the format 'r X Y', where X and Y are numerical values or alterations.", ":(", 0)
+                return false
+            end
+        elseif #params == 2 then
+            -- Handling 'r X'
+            local xInput = params[2]
+            local xValue = parsePitchValue(xInput)
+
+            if xValue then
+                local halfRange = xValue / 2
+                local minValue = 8192 - halfRange
+                local maxValue = 8192 + halfRange
+                return {type = 'random', minValue = minValue, maxValue = maxValue}
+            else
+                reaper.ShowMessageBox("Invalid input! \n\nPlease enter the command in the format 'r X', where X is a numerical value or alteration.", ":(", 0)
+                return false
+            end
+        elseif #params == 1 then
+            -- Handling 'r'
+            return {type = 'random', minValue = 1, maxValue = 16383}
+        else
+            reaper.ShowMessageBox("Invalid input! \n\nFor random pitch bend, enter 'r', 'r X', or 'r X Y', where X and Y are numerical values or alterations.", ":(", 0)
+            return false
+        end
+    elseif #params == 3 and params[2] == 'to' then
+        -- Handling 'X to Y'
+        local xInput = params[1]
+        local yInput = params[3]
+        local startValue = parsePitchValue(xInput)
+        local endValue = parsePitchValue(yInput)
+
+        if startValue and endValue then
+            return {type = 'slide', startValue = startValue, endValue = endValue}
+        else
+            reaper.ShowMessageBox("Invalid input! \n\nPlease enter the command in the format 'X to Y', where X and Y are numerical values or alterations.", ":(", 0)
+            return false
+        end
     else
-        reaper.ShowMessageBox("Invalid input! \n \nPlease enter a valid number between 0 and 16383 or next accidentals: bb, b/b, b, /b, 0, /#, #, #/#, ##;  also b or # nat.", ":(", 0)
-        return false
+        -- Handling fixed value
+        local bendValue = parsePitchValue(userInput)
+        if bendValue then
+            return {type = 'fixed', value = bendValue}
+        else
+            reaper.ShowMessageBox("Invalid input! \n\nPlease enter a numerical value between 0 and 16383 or one of the following alterations: bb, b/b, b, /b, 0, /#, #, #/#, ##; also b nat or # nat.", ":(", 0)
+            return false
+        end
     end
 end
 
 function focusMidiEditor()
-  local focus_midi_editor = reaper.NamedCommandLookup("_SN_FOCUS_MIDI_EDITOR")
-  reaper.Main_OnCommand(focus_midi_editor, 0)
+    local focus_midi_editor = reaper.NamedCommandLookup("_SN_FOCUS_MIDI_EDITOR")
+    reaper.Main_OnCommand(focus_midi_editor, 0)
 end
 
-local function main()    
+local function main()
     switchToPitch()
     shortenSelectedNotes()
 
     while true do
-        local bendValue = getUserInput()
-        if bendValue == nil then 
+        local bendValueData = getUserInput()
+        if bendValueData == nil then 
             lengthenSelectedNotes()
             return
-        elseif bendValue ~= false then 
-            applyPitchBend(bendValue)
+        elseif bendValueData ~= false then 
+            applyPitchBend(bendValueData)
             break
         end
     end
