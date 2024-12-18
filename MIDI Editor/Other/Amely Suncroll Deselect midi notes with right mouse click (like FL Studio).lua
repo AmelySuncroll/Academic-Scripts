cd0f4e6ac9b5cc17dbf546fb36e17f20d407cb35
Amely Suncroll <164076705+AmelySuncroll@users.noreply.github.com>
2024-12-18 22:08:07 +0400

    Update Amely Suncroll Deselect midi notes with right mouse click (like FL Studio).lua

--- MIDI Editor/Other/Amely Suncroll Deselect midi notes with right mouse click (like FL Studio).lua
+++ MIDI Editor/Other/Amely Suncroll Deselect midi notes with right mouse click (like FL Studio).lua
@@ -1,16 +1,21 @@
 -- @description Deselect midi notes with right mouse click (like FL Studio)
--- @version 1.04
+-- @version 1.05
 -- @author Amely Suncroll
 -- @website https://forum.cockos.com/showthread.php?t=291012
 -- @changelog
 --    + Improved toolbar icon handling
 --    + Remove "working" and "stopped" messages
 --    + Fix unmute notes if was muted
+--    + Changed treshold to 0.1 and add reaper's native deselecting function
 -- @about Click right mouse click just one time to deselect all midi notes. Hold right mouse click to delete midi notes (if you set up delete notes by right drag) and keep others selected.
 
 -- @donation https://www.paypal.com/paypalme/suncroll
 -- @website https://t.me/reaper_ua
 -- Support: https://t.me/amely_suncroll_support amelysuncroll@gmail.com
+
+if not reaper.APIExists('JS_ReaScriptAPI_Version') then
+    reaper.ShowMessageBox("Requies js_ReaScriptAPI.", ":(", 0)
+end
 
 function deselect_all_notes(midi_editor)
     local take = reaper.MIDIEditor_GetTake(midi_editor)
@@ -27,7 +32,7 @@ end
 
 local was_right_button_pressed = false
 local right_button_click_time = 0
-local right_button_held_threshold = 0.2
+local right_button_held_threshold = 0.1
 local command_id = ({reaper.get_action_context()})[4]
 local is_running = true
 
@@ -43,7 +48,8 @@ function check_right_click()
         if click_duration < right_button_held_threshold then
             local midi_editor = reaper.MIDIEditor_GetActive()
             if midi_editor then
-                deselect_all_notes(midi_editor)
+                -- deselect_all_notes(midi_editor)
+                reaper.MIDIEditor_LastFocused_OnCommand(40214, false)
             end
         end
     end
@@ -53,7 +59,7 @@ end
 
 function main()
     if not is_running then
-        stop_script() -- явный вызов stop_script при завершении
+        stop_script()
         return
     end
     check_right_click()
@@ -61,13 +67,14 @@ function main()
 end
 
 function start_script()
-    reaper.SetToggleCommandState(0, command_id, 1) -- highlight toolbar button
+    is_running = true
+    reaper.SetToggleCommandState(0, command_id, 1)
     reaper.RefreshToolbar2(0, command_id)
     main()
 end
 
 function stop_script()
-    reaper.SetToggleCommandState(0, command_id, 0) -- remove highlight from toolbar button
+    reaper.SetToggleCommandState(0, command_id, 0)
     reaper.RefreshToolbar2(0, command_id)
     is_running = false
 end
