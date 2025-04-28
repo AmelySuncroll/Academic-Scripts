dae30d7e72a723e011f05cc4320c4cd7d90a4250
Amely Suncroll <164076705+AmelySuncroll@users.noreply.github.com>
2025-04-28 12:32:22 +0300

    Add files via upload

---
+++ Other/Global Startup Action Tool/List/Amely Suncroll Global Startup Action List 4.lua
@@ -0,0 +1,42 @@
+-- @description Global Startup Action List 4
+-- @author Amely Suncroll, Alex_Ik
+-- @version 1.1
+-- @changelog
+--    + init @
+--    + 1.1 add tabs
+-- @noIndex
+
+local r = reaper
+
+-- ID: 1
+local mainWindowActions = {'_______________________________________________________________________arrange_view',
+'   ',
+}
+
+-- ID: 2
+local midiEeditorActions = {'   ',
+'________________________________________________________________________midi_editor',
+'   ',
+}
+
+local function runMainWindowActions()
+for _, action in ipairs(mainWindowActions) do
+  r.Main_OnCommandEx(r.NamedCommandLookup(action), 0)
+end  
+end
+
+function runMidiEditorActions()
+if r.MIDIEditor_GetActive() == nil then
+  r.defer(runMidiEditorActions)
+else
+  local me = r.MIDIEditor_GetActive()
+  r.PreventUIRefresh(1)
+  for _, action in ipairs(midiEeditorActions) do
+    r.MIDIEditor_OnCommand(me, r.NamedCommandLookup(action), 0)
+  end
+  r.PreventUIRefresh(-1)
+end
+end
+
+r.defer(runMainWindowActions)
+r.defer(runMidiEditorActions)
