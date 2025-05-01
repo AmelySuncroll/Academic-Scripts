29f96b8a913c1a12900868ddef1a3d76f617c7d0
Amely Suncroll <164076705+AmelySuncroll@users.noreply.github.com>
2025-05-01 11:22:15 +0300

    Update Amely Suncroll Solo tracks (like Cubase).lua

--- Other/Arrange View/Amely Suncroll Solo tracks (like Cubase).lua
+++ Other/Arrange View/Amely Suncroll Solo tracks (like Cubase).lua
@@ -1,6 +1,6 @@
 -- @description Solo tracks (like Cubase)
 -- @author Amely Suncroll
--- @version 1.0
+-- @version 1.1
 -- @changelog
 --    + init @
 
@@ -19,34 +19,89 @@ function main()
     if num_selected == 0 then return end
 
     local selected_tracks = {}
+    
     for i = 0, num_selected - 1 do
         local track = reaper.GetSelectedTrack(0, i)
         table.insert(selected_tracks, track)
     end
 
-    local function tog_solo_parent(track)
+    local function is_any_child_soloed(folder_track)
+        local total_tracks = reaper.CountTracks(0)
+        local start_idx = reaper.GetMediaTrackInfo_Value(folder_track, "IP_TRACKNUMBER")
+        local depth = 1
+        
+        for i = start_idx, total_tracks - 1 do
+            local track = reaper.GetTrack(0, i)
+            local folder_depth = reaper.GetMediaTrackInfo_Value(track, "I_FOLDERDEPTH")
+
+            if reaper.GetMediaTrackInfo_Value(track, "I_SOLO") ~= 0 then
+                return true
+            end
+
+            depth = depth + folder_depth
+            if depth <= 0 then break end
+        end
+        
+        return false
+    end
+
+    local function tog_solo_related(track)
         local is_solo = reaper.GetMediaTrackInfo_Value(track, "I_SOLO")
         local new_val = (is_solo == 0) and 1 or 0
         reaper.SetMediaTrackInfo_Value(track, "I_SOLO", new_val)
 
-        if new_val == 1 then
-            local parent = reaper.GetParentTrack(track)
-            while parent ~= nil do
+        local parent = reaper.GetParentTrack(track)
+
+        while parent do
+            if new_val == 1 then
                 reaper.SetMediaTrackInfo_Value(parent, "I_SOLO", 1)
-                parent = reaper.GetParentTrack(parent)
+            else
+                if not is_any_child_soloed(parent) then
+                    reaper.SetMediaTrackInfo_Value(parent, "I_SOLO", 0)
+                end
             end
-        else
-            local parent = reaper.GetParentTrack(track)
-            
-            while parent ~= nil do
-                reaper.SetMediaTrackInfo_Value(parent, "I_SOLO", 0)
-                parent = reaper.GetParentTrack(parent)
+            parent = reaper.GetParentTrack(parent)
+        end
+
+        local num_sends = reaper.GetTrackNumSends(track, 0)
+
+        for i = 0, num_sends - 1 do
+            local dest_track = reaper.BR_GetMediaTrackSendInfo_Track(track, 0, i, 1)
+            if dest_track then
+                reaper.SetMediaTrackInfo_Value(dest_track, "I_SOLO", new_val)
+            end
+        end
+
+        local folder_depth = reaper.GetMediaTrackInfo_Value(track, "I_FOLDERDEPTH")
+
+        if folder_depth == 1 then
+            local total_tracks = reaper.CountTracks(0)
+            local start_idx = reaper.GetMediaTrackInfo_Value(track, "IP_TRACKNUMBER")
+            local depth = 1
+
+            for i = start_idx, total_tracks - 1 do
+                local sub_track = reaper.GetTrack(0, i)
+                local sub_depth = reaper.GetMediaTrackInfo_Value(sub_track, "I_FOLDERDEPTH")
+
+                reaper.SetMediaTrackInfo_Value(sub_track, "I_SOLO", new_val)
+
+                local num_sends = reaper.GetTrackNumSends(sub_track, 0)
+
+                for j = 0, num_sends - 1 do
+                    local dest_track = reaper.BR_GetMediaTrackSendInfo_Track(sub_track, 0, j, 1)
+                    if dest_track then
+                        reaper.SetMediaTrackInfo_Value(dest_track, "I_SOLO", new_val)
+                    end
+                end
+
+                depth = depth + sub_depth
+                if depth <= 0 then break end
             end
         end
     end
 
     for _, track in ipairs(selected_tracks) do
-        tog_solo_parent(track)
+        tog_solo_related(track)
     end
 
     local track_count = reaper.CountTracks(0)
@@ -54,7 +109,6 @@ function main()
 
     for i = 0, track_count - 1 do
         local track = reaper.GetTrack(0, i)
-
         if reaper.GetMediaTrackInfo_Value(track, "I_SOLO") ~= 0 then
             any_soloed = true
             break
@@ -64,7 +118,7 @@ function main()
     reaper.PreventUIRefresh(1)
 
     if any_soloed then
-        for i = 0, track_count -1 do
+        for i = 0, track_count - 1 do
             local track = reaper.GetTrack(0, i)
             if reaper.GetMediaTrackInfo_Value(track, "I_SOLO") == 0 then
                 reaper.SetMediaTrackInfo_Value(track, "B_MUTE", 1)
@@ -73,7 +127,7 @@ function main()
             end
         end
     else
-        for i = 0, track_count -1 do
+        for i = 0, track_count - 1 do
             local track = reaper.GetTrack(0, i)
             reaper.SetMediaTrackInfo_Value(track, "B_MUTE", 0)
         end
@@ -83,3 +137,4 @@ function main()
 end
 
 main()
+
