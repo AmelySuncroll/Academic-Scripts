8b889e25c0ff0931bde959e3fdcb63eb26eeff62
Amely Suncroll <164076705+AmelySuncroll@users.noreply.github.com>
2026-02-11 01:40:59 +0400

    Add files via upload

---
+++ Other/Amely Suncroll Ukrainian Localization (auto update tool).lua
@@ -0,0 +1,69 @@
+-- @description Ukrainian Localization (auto update tool)
+-- @author Amely Suncroll
+-- @version 1.0
+-- @website https://t.me/reaper_ua
+-- @changelog
+--    + init @
+
+-- @about Add this script to autorun when you start REAPER: 1. Розширення / SWS/S&M / Startup actions / Set global startup action OR 2. Get 'Global Startup Action Tool' script in ReaPack and follow instructions.
+    
+-- @donation https://www.paypal.com/paypalme/suncroll
+
+-- Support:
+-- https://t.me/amely_suncroll_support
+-- amelysuncroll@gmail.com
+-- https://github.com/AmelySuncroll
+
+
+
+local url_file = "https://raw.githubusercontent.com/AmelySuncroll/ReaperLangPackUA/main/Ukrainian%20for%20v7.55+.ReaperLangPack"
+local url_api = "https://api.github.com/repos/AmelySuncroll/ReaperLangPackUA/commits/main"
+local langpack_name = "Ukrainian for v7.55+.ReaperLangPack"
+
+function Msg(val)
+    reaper.ShowConsoleMsg(tostring(val) .. "\n")
+end
+
+local function fetch(cmd)
+    local handle = io.popen(cmd)
+    local result = handle:read("*a")
+    handle:close()
+    return result
+end
+
+function UpdateLocalization()
+    local os_name = reaper.GetOS()
+    local separator = package.config:sub(1,1)
+    local full_path = reaper.GetResourcePath() .. separator .. "LangPack" .. separator .. langpack_name
+    local api_cmd = string.format('curl -s -H "User-Agent: Reaper-Script" "%s"', url_api)
+    local response = fetch(api_cmd)
+    local new_version = response:match('"message":%s*"(.-)"')
+    
+    if not new_version then 
+        local ret = reaper.MB("Не вдалося перевірити версію. Оновити файл локалізації примусово?", "Увага", 4)
+        if ret ~= 6 then return end
+    else 
+        local msg_text = string.format("Добрий день!\n\nДоступна нова версія локалізації: %s\n\nБажаєте завантажити оновлення?", new_version)
+        local ret = reaper.MB(msg_text, "Тук-тук", 4) -- 4 = Yes/No
+        if ret ~= 6 then 
+            return 
+        end
+    end
+
+    local dl_cmd
+    if os_name:match("Win") then
+        dl_cmd = string.format('curl -L -s "%s" -o "%s" -w "%%{http_code}"', url_file, full_path)
+    else
+        dl_cmd = string.format("curl -L -s '%s' -o '%s' -w '%%{http_code}'", url_file, full_path)
+    end
+
+    local http_code = fetch(dl_cmd):match("%d+")
+
+    if http_code == "200" then
+        reaper.MB("Локалізацію успішно оновлено!\nБудь ласка, перезапустіть Reaper, щоб застосувати зміни.", "Готово", 0)
+    else
+        reaper.MB("Не вдалося завантажити файл. Код: " .. (http_code or "nil"), "Помилка :(", 0)
+    end
+end
+
+UpdateLocalization()
